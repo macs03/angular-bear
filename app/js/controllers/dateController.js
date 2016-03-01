1f7dcf4c57d7ded995ce74227c2027a866c23ddb
Orlando Becerra <orlandobcrra@gmail.com>
2016-03-01 11:17:24 -0430

    new design of licoFinder

--- app/js/controllers/dateController.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var app = angular.module('licoFinder.dateController',[]);
-
-    app.controller('dateController', ["$scope",function ($scope) {
-        var currentDate = new Date();
-        $scope.year = currentDate.getUTCFullYear();
-        console.log($scope.year);
-    }]);
-})();
